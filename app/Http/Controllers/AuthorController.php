27ea8d8e10ab0dd125b2f59114d601bec53e4dae
Saad Amjad <saad.amjad@monstar-lab.com>
2021-08-28 15:25:24 +0600

    feat: added book and author controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Author::all();
+    }
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Book::all();
+    }
+}
