f499946fd31763aef98e28cb02957005a089e1c3
Saad Amjad <saad.amjad@monstar-lab.com>
2021-08-28 15:29:24 +0600

    feat: added book and author factories

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->catchPhrase,
+        ];
+    }
+}
