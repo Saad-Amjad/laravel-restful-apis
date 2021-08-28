faff7eb40dabac4988208f66f33541445715424c
Saad Amjad <saad.amjad@monstar-lab.com>
2021-08-28 15:27:18 +0600

    feat: added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/authors', [AuthorController::class, 'index']);
 });
