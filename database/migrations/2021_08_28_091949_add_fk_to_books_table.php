95d1c16e26cfb5dbfd0ddad3574cfc3d1f0f2fab
Saad Amjad <saad.amjad@monstar-lab.com>
2021-08-28 15:21:19 +0600

    feat: added books and author tables

---
+++ database/migrations/2021_08_28_091949_add_fk_to_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFkToBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->unsignedBigInteger('author_id')->after('title');
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropForeign('books_author_id_foreign');
+            $table->dropColumn('author_id');
+        });
+    }
+}
